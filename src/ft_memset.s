3126de6b00208a12f382cdc947e204eafe92339d
arwn <windham.aren@protonmail.com>
2019-06-08 14:11:43 -0700

    clean memset

--- src/ft_memset.s
+++ src/ft_memset.s
@@ -1,10 +1,18 @@
+;;Author: Aren Windham
+;;The ft_memset function sets len bytes in b to c.
+
+;;void *ft_memset(void *b, int c, int len)
 global _ft_memset
 
 section .text
 _ft_memset:
-	dec rdx
-	mov byte [rdi + rdx], sil
-	cmp rdx, 0
-	jne _ft_memset
-	mov rax, rdi
-	ret
+    cmp     rdx, 0                  ;if len <= 0 then done
+    jle     done
+memset:                             ;while len > 0
+    dec     rdx                     ;len--
+    mov     byte [rdi + rdx], sil   ;b+len = c
+    cmp     rdx, 0                  ;if c <= 1
+    jg      memset                  ;then done
+done:
+    mov     rax, rdi
+    ret
