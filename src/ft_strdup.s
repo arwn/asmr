caf0fb3f280c861afab4a3e40e044d83ab48b158
arwn <windham.aren@protonmail.com>
2019-06-08 14:41:16 -0700

    clean strdup

--- src/ft_strdup.s
+++ src/ft_strdup.s
@@ -1,3 +1,8 @@
+;;Author: Aren Windham
+;;The ft_strdup function allocates space for a new string with the
+;;length of strlen(s)+1 and copies the data into the return value.
+
+;;char *ft_strdup(char *s)
 global _ft_strdup
 extern _malloc
 extern _ft_memcpy
@@ -5,14 +10,26 @@ extern _ft_strlen
 
 section .text
 _ft_strdup:
-	push rdi ;save from strlen
-	call _ft_strlen
-	inc rax
-	push rax
-	mov rdi, rax
-	call _malloc
-	mov rdi, rax ;dest
-	pop rdx ;length
-	pop rsi ;src string
-	call _ft_memcpy
+	;align stack since we use _malloc
+	push	rbp
+	mov		rbp, rsp
+	sub		rsp, 16
+
+	push	rdi         ;save *s from clobbering
+	call	_ft_strlen
+	inc		rax         ;strlen+1 for null terminator
+
+	mov		rdi, rax    ;malloc strlen+1
+	push	rdi         ;save size from clobbering
+	call	_malloc
+	cmp		rax, 0      ;if malloc failed then
+	je		done        ;done
+
+	mov		rdi, rax    ;memcpy into malloc return
+	pop		rdx         ;memcpy from *s
+	pop		rsi         ;memcpy size of strlen+1
+	call	_ft_memcpy
+
+done:
+	leave
 	ret
