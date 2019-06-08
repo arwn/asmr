51410f9a5cc33da7321f55e696acc7b92f66b8b7
arwn <windham.aren@protonmail.com>
2019-06-08 14:24:03 -0700

    clean puts

--- src/ft_puts.s
+++ src/ft_puts.s
@@ -1,33 +1,37 @@
+;;Author: Aren Windham
+;;The ft_puts function writes string s to the standard output and
+;;appends a newline charater.
+
+;;void ft_puts(char *s)
 global _ft_puts
 extern _ft_strlen
 
 section .data
-	nl db 10
-	nulltext db "(null)", 0
+    nl       db 10          ;newline character
+    nulltext db "(null)", 0
 
 section .text
 _ft_puts:
-	cmp rdi, 0 ;print (null) if passed a null pointer
-	jnz notnull
-	mov rdi, nulltext
+    cmp     rdi, 0          ;if *s != nil then
+    jnz     notnull         ;notnull
+    mov     rdi, nulltext   ;move nulltext into *s
 notnull:
-	push rdi ;prevent clobbering from strlen
-	call _ft_strlen
-	pop rdi
-	push rax ;save for return value of puts
-print:
-	mov rsi, rdi ;string
-	mov rdi, 1 ;fd
-	mov rdx, rax ;len
-	mov rax, 0x2000004 ;write
-	syscall
-	jmp newline
-newline:
-	mov rsi, nl
-	mov rdi, 1
-	mov rdx, 1
-	mov rax, 0x2000004
-	syscall
-	pop rax
-	inc rax
-	ret
+    push    rdi             ;prevent clobbering from strlen
+    call    _ft_strlen
+    pop     rdi
+    push    rax             ;save for return value of puts
+print_string:
+    mov     rsi, rdi        ;string
+    mov     rdi, 1          ;fd
+    mov     rdx, rax        ;len
+    mov     rax, 0x2000004  ;write syscall
+    syscall
+print_newline:
+    mov     rsi, nl         ;string
+    mov     rdi, 1          ;stdout
+    mov     rdx, 1          ;one byte
+    mov     rax, 0x2000004  ;write syscall
+    syscall
+    pop     rax
+    inc     rax             ;inc because newline
+    ret
