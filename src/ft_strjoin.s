1cbb44e05fbb200e5d851caf7a98f33b99a83efa
arwn <windham.aren@protonmail.com>
2019-06-08 16:37:42 -0700

    strjoin kinda

--- src/ft_strjoin.s
+++ src/ft_strjoin.s
@@ -1,3 +1,8 @@
+;;Author: Aren Windham
+;;The ft_strjoin function returns a new string containing s1
+;;concatenated with s2.
+
+;;char *ft_strjoin(char *s1, char *s2)
 global _ft_strjoin
 
 extern _malloc
@@ -7,26 +12,30 @@ extern _ft_bzero
 
 section .text
 _ft_strjoin:
-    push rdi ; s1
-    push rsi ; s2
+    push    rdi             ;s1
+    push    rsi             ;s2
 lengths:
-    call _ft_strlen
-    mov r8, rax ;len s1
-    mov rdi, [rsp]
-    call _ft_strlen
-    mov r9, rax ;len s2
-    add r8, r9
-    add r8, 1
-    mov rdi, r8
+    call    _ft_strlen
+    push    rax             ;save s1 len
+    mov     rdi, [rsp+8]    ;s2
+    call    _ft_strlen
+    pop     r8              ;pop len1 into r8
+    mov     r9, rax         ;len s2 into r9
+    add     r8, r9          ;len +=1
+    add     r8, 1
+    mov     rdi, r8
 copy:
-    call _malloc
-    mov rsi, rdi
-    mov rdi, rax
-    call _ft_bzero
-    mov rsi, [rsp + 8]
-    call _ft_strcat
-    mov rdi, rax
-    mov rsi, [rsp]
-    call _ft_strcat
+    call    _malloc         ;malloc new space
+    mov     rsi, rdi
+    mov     rdi, rax
+    call    _ft_bzero       ;bzero maloced space
+    mov     rsi, [rsp + 8]
+    call    _ft_strcat
+    mov     rdi, rax
+    mov     rsi, [rsp]
+    call    _ft_strcat
 	leave
     ret
+
+;;bugs
+;malloc not called correctly
