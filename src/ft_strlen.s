9a76fad90d1b94cd73e1d95378de70c8f114c973
arwn <windham.aren@protonmail.com>
2019-06-08 14:41:24 -0700

    no dot

--- src/ft_strlen.s
+++ src/ft_strlen.s
@@ -7,8 +7,8 @@ global _ft_strlen
 section .text
 _ft_strlen:
 	mov		rax, -1				;init to -1 so we start at 0
-.cstr_iter:
+cstr_iter:
 	inc		rax					;size++
 	cmp		byte [rdi + rax], 0	;if *s == '\0' then return size
-	jne		.cstr_iter
+	jne		cstr_iter
 	ret
