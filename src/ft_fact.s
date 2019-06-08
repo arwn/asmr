2517cd46bded570dfea9bf5a2475ff99fabf8451
arwn <windham.aren@protonmail.com>
2019-06-08 12:55:01 -0700

    clean fact

--- src/ft_fact.s
+++ src/ft_fact.s
@@ -1,13 +1,18 @@
+;;Author: Aren Windham
+;;The ft_fact program calculates the factorial of n where n is a
+;;positive integer.
+
+;;int ft_fact(int n)
 global _ft_fact
 
 section .text
 _ft_fact:
-	mov rax, 1
+	mov	rax, 1	;fact(1) is 1 so start with that
 fact:
-	cmp rdi, 1
-	jle done
-	mul rdi
-	dec rdi
-	jmp fact
+	cmp	rdi, 1	;if n <= 1
+	jle	done	;then done
+	mul	rdi		;rax *= n
+	dec	rdi		;n--
+	jmp	fact
 done:
 	ret
