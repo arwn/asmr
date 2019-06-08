75bddaa3c26ce519d67b31597dc122c5e7cba541
arwn <windham.aren@protonmail.com>
2019-06-08 13:47:45 -0700

    clean isprint

--- src/ft_isprint.s
+++ src/ft_isprint.s
@@ -1,14 +1,17 @@
+;;Author: Aren Windham
+;;The ft_isprint function returns 1 if c is printable to a terminal.
+
+;;int ft_isprint(int c)
 global _ft_isprint
 
 section .text
 _ft_isprint:
-	cmp rdi, 0x20
-	jl false
-	cmp rdi, 0x7e
-	jg false
-	mov rax, 1
-	ret
-
+    cmp     rdi, 0x20   ;if c < 'sp'
+    jl      false       ;then false
+    cmp     rdi, 0x7e   ;if c > '~'
+    jg      false       ;then false
+    mov     rax, 1      ;else true
+    ret
 false:
-	mov rax, 0
-	ret
+    mov     rax, 0
+    ret
