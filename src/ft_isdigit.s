c4f89d106fe49885953c7bbcf82a36ca4f86f765
arwn <windham.aren@protonmail.com>
2019-06-08 13:44:11 -0700

    clean isdigit

--- src/ft_isdigit.s
+++ src/ft_isdigit.s
@@ -1,15 +1,17 @@
+;;Author: Aren Windham
+;;The ft_isdigit function returns 1 if c is a digit between '0' and '9'.
+
+;;int ft_isdigit(int c)
 global _ft_isdigit
- 
+
 section .text
 _ft_isdigit:
-	cmp		rdi, 48
-	jl		.not_digit
-	cmp		rdi, 57
-	jg		.not_digit
-
-	mov 	eax, 1
+	cmp		rdi, '0'		;if c < '0'
+	jl		not_digit		;then false
+	cmp		rdi, '9'		;else if c > '9'
+	jg		not_digit		;then false
+	mov 	eax, 1			;else true
 	ret
-
-.not_digit:
+not_digit:
 	mov 	eax, 0
 	ret
