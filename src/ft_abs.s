a888c2c8adf9b408924de56a882042051874ba9a
arwn <windham.aren@protonmail.com>
2019-06-07 21:41:25 -0700

    abs cleanup

--- src/ft_abs.s
+++ src/ft_abs.s
@@ -1,10 +1,13 @@
+;;Author: A.R.W
+;;This program calculates the absolute value of a signed integer
+;;int ft_abs(int number);
 global _ft_abs
 
 section .text
 _ft_abs:
-	cmp edi, 0
-	mov eax, edi
-	jg done
-	imul eax, -1
+	mov		eax, edi
+	cmp		eax, 0 		;is number < 0
+	jge		done
+	imul	eax, -1		;multiply by -1
 done:
 	ret
