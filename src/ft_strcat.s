f730212c2a5ea591b028887218550ad78b021f4e
arwn <windham.aren@protonmail.com>
2019-06-08 14:48:35 -0700

    clean strdup

--- src/ft_strcat.s
+++ src/ft_strcat.s
@@ -1,23 +1,27 @@
+;;Author: Aren Windham
+;;The ft_strcat function concatenates s2 onto s1 without malloc.
+
+;;char *ft_strcat()
 extern _ft_strlen
 extern _ft_memcpy
 global _ft_strcat
 
 section .text
 _ft_strcat:
-	push	rdi ;output
-	push	rdi ;dest
-	push	rsi ;src
-	call	_ft_strlen
-	pop		rdi ;prepare argument
-	push	rax ;desclen
-	push	rdi ;src
-	call	_ft_strlen
-	pop		rsi ;src
-	pop		rdi ;desclen
-	pop		rcx ;dec
-	add		rdi, rcx ;dest for memcpy
-	mov		rdx, rax
-	inc		rdx ;null term.
-	call	_ft_memcpy
-	pop		rax
-	ret
+    push	rdi         ;output
+    push	rdi         ;dest
+    push	rsi         ;src
+    call	_ft_strlen
+    pop		rdi         ;prepare argument
+    push	rax         ;desclen
+    push	rdi         ;src
+    call	_ft_strlen
+    pop		rsi         ;src
+    pop		rdi         ;desclen
+    pop		rcx         ;dec
+    add		rdi, rcx    ;dest for memcpy
+    mov		rdx, rax
+    inc		rdx         ;null term
+    call	_ft_memcpy
+    pop		rax
+    ret
