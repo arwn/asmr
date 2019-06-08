4eeea823e3db8ca722ad886fa714057e67566893
arwn <windham.aren@protonmail.com>
2019-06-07 22:52:54 -0700

    clean tolower

--- src/ft_tolower.s
+++ src/ft_tolower.s
@@ -1,12 +1,23 @@
+;;Author: Aren Windham
+
+;;Description
+;the tolower funcion converts an upper-case letter to the coresponding
+;lower-case letter.
+
+;;Register Usage
+;rdi	may be incremented by 32
+;rax	return value
+
+;;int ft_tolower(int c)
 global _ft_tolower
 
 section .text
 _ft_tolower:
-	cmp rdi, 'A'
+	cmp rdi, 'A'	;if c <= 'A' then done
 	jl done
-	cmp rdi, 'Z'
+	cmp rdi, 'Z'	;if c <= 'Z' then done
 	jg done
-	add rdi, 32
+	add rdi, 32		;else c += 32
 done:
 	mov rax, rdi
 	ret
--- src/ft_toupper.s
+++ src/ft_toupper.s
@@ -8,11 +8,6 @@
 ;rdi	may have 32 subtracted
 ;rax	return value
 
-;;pseudocode
-;if c < 'a' then return c
-;else if c > 'z' then return c
-;else return c - 32 end
-
 ;;int toupper(int c)
 global _ft_toupper
 
@@ -20,9 +15,9 @@ section .text
 _ft_toupper:
 	cmp	rdi, 'a'	;if c < 'a' then done
 	jl	done
-	cmp	rdi, 'z'	;if c > 'z' then done
+	cmp	rdi, 'z'	;else if c > 'z' then done
 	jg	done
-	sub	rdi, 32		;c -= 32
+	sub	rdi, 32		;else c -= 32
 done:
 	mov	rax, rdi
 	ret
